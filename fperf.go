4deeed88fc0812e3ce6fe0dae1e1da7f59b0edde
Shafreeck Sea <shafreeck@gmail.com>
2016-04-14 18:48:39 +0800

    Add command documentation

--- fperf.go
+++ fperf.go
@@ -1,5 +1,47 @@
 package main
 
+/*
+### Three steps to create your own testcase
+1.Create the "NewClient" function
+
+```go
+package demo
+
+import (
+	"fmt"
+	"github.com/shafreeck/fperf/client"
+	"time"
+)
+
+type DemoClient struct{}
+
+func NewDemoClient(flag *client.FlagSet) client.Client {
+	return &DemoClient{}
+}
+```
+
+2.Implement the UnaryClient or StreamClient
+```go
+func (c *DemoClient) Dial(addr string) error {
+	fmt.Println("Dial to", addr)
+	return nil
+}
+
+func (c *DemoClient) Request() error {
+	time.Sleep(100 * time.Millisecond)
+	return nil
+}
+```
+
+3.Register to fperf
+```go
+func init() {
+	client.Register("demo", NewDemoClient, "This is a demo client discription")
+}
+```
+
+*/
+
 import (
 	"flag"
 	"fmt"
