8e4c554ba8e7809dffe02979c7547c9259185c6e
Shafreeck Sea <shafreeck@gmail.com>
2018-10-16 10:09:11 +0800

    Add __rand_int__, __seq_int__ support

--- testcases/redis/redis.go
+++ testcases/redis/redis.go
@@ -17,9 +17,18 @@ package redis
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/garyburd/redigo/redis"
 	"github.com/shafreeck/fperf/client"
 )
+
+var seq func() string = seqCreater(0)
+var random func() string = randCreater(10000000000000000)
 
 //A test case can have itself options witch would be passed by fperf
 type options struct {
@@ -41,7 +50,7 @@ func newRedisClient(flag *client.FlagSet) client.Client {
 
 	//Customize the usage output
 	flag.Usage = func() {
-		fmt.Printf("Usage: redis [options] [cmd] [args...]\noptions:\n")
+		fmt.Printf("Usage: redis [options] [cmd] [args...], use __rand_int__ or __seq_int__ to generate random or sequence keys\noptions:\n")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -69,6 +78,85 @@ func (c *redisClient) Dial(addr string) error {
 	return nil
 }
 
+func seqCreater(begin int64) func() string {
+	// filled map, filled generated to 16 bytes
+	l := []string{
+		"",
+		"0",
+		"00",
+		"000",
+		"0000",
+		"00000",
+		"000000",
+		"0000000",
+		"00000000",
+		"000000000",
+		"0000000000",
+		"00000000000",
+		"000000000000",
+		"0000000000000",
+		"00000000000000",
+		"000000000000000",
+	}
+	v := begin
+	m := &sync.Mutex{}
+	return func() string {
+		m.Lock()
+		s := strconv.FormatInt(v, 10)
+		v += 1
+		m.Unlock()
+
+		filled := len(l) - len(s)
+		if filled <= 0 {
+			return s
+		}
+		return l[filled] + s
+	}
+}
+
+func randCreater(max int64) func() string {
+	// filled map, filled generated to 16 bytes
+	l := []string{
+		"",
+		"0",
+		"00",
+		"000",
+		"0000",
+		"00000",
+		"000000",
+		"0000000",
+		"00000000",
+		"000000000",
+		"0000000000",
+		"00000000000",
+		"000000000000",
+		"0000000000000",
+		"00000000000000",
+		"000000000000000",
+	}
+	var v int64
+	m := &sync.Mutex{}
+	return func() string {
+		m.Lock()
+		v = rand.Int63n(max)
+		s := strconv.FormatInt(v, 10)
+		m.Unlock()
+
+		filled := len(l) - len(s)
+		if filled <= 0 {
+			return s
+		}
+		return l[filled] + s
+	}
+}
+
+func replaceSeq(s string) string {
+	return strings.Replace(s, "__seq_int__", seq(), -1)
+}
+func replaceRand(s string) string {
+	return strings.Replace(s, "__rand_int__", random(), -1)
+}
+
 //Request send a redis request and return the error if there is
 func (c *redisClient) Request() error {
 	var args []interface{}
@@ -76,6 +164,12 @@ func (c *redisClient) Request() error {
 	//Build the redis cmd and args
 	cmd := c.args[0]
 	for _, arg := range c.args[1:] {
+		if strings.Index(arg, "__seq_int__") >= 0 {
+			arg = replaceSeq(arg)
+		}
+		if strings.Index(arg, "__rand_int__") >= 0 {
+			arg = replaceRand(arg)
+		}
 		args = append(args, arg)
 	}
 	_, err := c.rds.Do(cmd, args...)
@@ -84,5 +178,6 @@ func (c *redisClient) Request() error {
 
 //Register to fperf
 func init() {
+	rand.Seed(time.Now().UnixNano())
 	client.Register("redis", newRedisClient, "redis performance benchmark")
 }
